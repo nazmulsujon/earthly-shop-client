14af6920d1adf9bb7469cdf3a2d44d4532fd5f42
Nazmul Hoque <108403105+nazmulsujon@users.noreply.github.com>
2024-10-02 16:23:15 +0600

    Update Map.js

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -17,7 +17,7 @@ const position = [23.7956, 90.3537];
 
 const Map = () => {
   return (
-    <div className="my-10">
+    <div className="my-10 z-10">
       <MapContainer className="mx-auto rounded w-[300px] h-[300px] lg:w-[90%] lg:h-[400px]" center={position} zoom={12}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         <Marker position={position}>
